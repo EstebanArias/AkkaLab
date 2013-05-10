d3bb83df91e3fe74be10c00770328f40ab51da7c
EstebanArias <esariasgo@unal.edu.co>
2013-05-10 14:12:35 -0500

    Commit Número1

---
+++ AkkaLab/src/akka/Akkatutorial.java
@@ -0,0 +1,19 @@
+package akka;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+public class Akkatutorial {
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		ActorSystem system=ActorSystem.create("Hola");
+		ActorRef helloActor = system.actorOf(new Props(HelloActor.class), "hola");
+		helloActor.tell("hello",null);
+		helloActor.tell("chao",null);
+		system.shutdown();
+		system.awaitTermination();//JOIN
+	}
+}
